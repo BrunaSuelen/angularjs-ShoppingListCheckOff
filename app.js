58cff79afffc0e049558bc7b606567f92a97f70c
C1324737 Bruna Suelen Garcia Rosa Silva <c1324737@interno.bb.com.br>
2022-02-11 13:36:00 -0300

    feat: Implementa a funcionalidade de adição e subtração de itens das listas

--- app.js
+++ app.js
@@ -8,51 +8,58 @@
     .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
 
+
   // Controller ToBuyController
-  ToBuyController.$inject = ['$scope'];
-  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  ToBuyController.$inject = [
+    '$scope', 
+    'ShoppingListCheckOffService'
+  ];
 
   function ToBuyController($scope, ShoppingListCheckOffService) {
-    // $scope.lunch = '';
-    // $scope.message = '';
-
-    // $scope.checkIfTooMuch = function() {
-    //   const LUNCH_ITEMS = $scope.separateLunchItems();
-
-    //   if (!LUNCH_ITEMS.length) {
-    //     return $scope.message = 'Please enter data first';
-    //   }
-
-    //   $scope.message = LUNCH_ITEMS.length <= 3 
-    //     ? 'Enjoy!'
-    //     : 'Too much!';
-    // }
-
-    // $scope.separateLunchItems = function() {
-    //   return $scope.lunch
-    //     .split(' ')
-    //     .join('')
-    //     .split(',')
-    //     .filter(function(item) {
-    //       return item != '' && item != ' ';
-    //     });
-    // }
+    $scope.buyItem = function(item) {
+      ShoppingListCheckOffService.addItem(item);
+      ShoppingListCheckOffService.removeItem(item.name)
+    }
+ 
+    $scope.getToBuy = function () {
+      return ShoppingListCheckOffService.toBuy;
+    }
   }
+
 
 
   // Controller AlreadyBoughtController
-  AlreadyBoughtController.$inject = ['$scope'];
-  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  AlreadyBoughtController.$inject = [
+    '$scope', 
+    'ShoppingListCheckOffService'
+  ];
 
   function AlreadyBoughtController($scope, ShoppingListCheckOffService) {
-
+    $scope.getAlredyBought = function() {
+      return ShoppingListCheckOffService.alreadyBought;
+    }
   }
+
 
 
   // Service ShoppingListCheckOffService
   function ShoppingListCheckOffService() {
-    this.addItem = function() {
+    this.alreadyBought = [];
 
+    this.toBuy = [
+      { name: 'Cookie', quantity: 2 },
+      { name: 'Apple', quantity: 5 },
+      { name: 'Watermelon', quantity: 1 },
+      { name: 'Meat', quantity: 3 },
+      { name: 'Cheetos', quantity: 10 }
+    ];
+
+    this.addItem = function(item) {
+      this.alreadyBought.push(item);
+    }
+
+    this.removeItem = function(name) {
+      this.toBuy = this.toBuy.filter(item => item.name !== name)
     }
   }
 }
